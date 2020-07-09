c9ae5055915724e9fffb511bf765a22ec7c7c7fb
colsonisaiah <colson.isaiah@gmail.com>
2020-07-09 18:32:53 -0400

    add token to controllers auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,7 +40,18 @@ const login = async (req,res) => {
 
         const isMatch = await bcrypt.compare(req.body.password, foundUser.password);
         if (isMatch) {
-            // TODO
+            const signedJwt = await jwt.sign(
+                { _id: foundUser._id},
+                'rubberduckig11',
+                {
+                    expiresIn: '1h'
+                }
+            );
+            res.status(200),json({
+                status:200,
+                message: "Success",
+                token: signedJwt
+            });
         } else {
             return res.status(400).json({
                 status: 400,
@@ -55,7 +66,21 @@ const login = async (req,res) => {
     }
 }
 
+const profile = async (req,res) => {
+    try {
+        const foundUser = await db.User.findById(req.currentUser);
+
+        res.json({headers: req.headers, user:foundUser});
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            message: "Something went wrong, please try again"
+        });
+    }
+}
+
 module.exports = {
     register,
     login,
+    profile,
 }
