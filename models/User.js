876c97ac160e6e40780352acf9e27484b7f88148
colsonisaiah <colson.isaiah@gmail.com>
2020-07-09 12:40:55 -0400

    added User model

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        firstName: {type: String, required: true},
+        lastName: {type: String, required: true},
+        username: {type: String, required: true, unique: true},
+        email: {type: String, required: true, unique: true},
+        password: {type: String, required: true}
+    }, {
+        timestamps: true
+    }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
