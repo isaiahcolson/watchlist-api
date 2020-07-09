8f0514105bb43c42b0528fafa4eb7ca5df745ff6
colsonisaiah <colson.isaiah@gmail.com>
2020-07-09 18:37:56 -0400

    update routes with authRequired

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,12 @@
 const router = require('express').Router();
 const controller = require('../controllers');
 
+const authRequired = require('../middleware/authRequired');
+
 // routes
 router.post('/register', controller.auth.register);
 router.post('/login', controller.auth.login);
+router.get('/profile', authRequired, controller.auth.profile);
 
 // exports
 module.exports = router;
--- routes/titles.js
+++ routes/titles.js
@@ -2,9 +2,11 @@
 const router = require('express').Router();
 const controller = require('../controllers');
 
+const authRequired = require('../middleware/authRequired');
+
 // routes
-router.get('/', controller.titles.index);
-router.get('/:id', controller.titles.show);
+router.get('/', authRequired, controller.titles.index);
+router.get('/:id', authRequired, controller.titles.show);
 
 // exports
 module.exports = router;
