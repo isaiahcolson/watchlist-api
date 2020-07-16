20535dc7177d917766b374764736aa0bb63c43cb
colsonisaiah <colson.isaiah@gmail.com>
2020-07-16 18:05:58 -0400

    manual ordering for watchlist

--- controllers/watchlists.js
+++ controllers/watchlists.js
@@ -30,7 +30,7 @@ const update = (req,res) => {
     });
 }
 
-// update route
+// update route by removing title
 const updateRemove = (req,res) => {
     db.Watchlist.findByIdAndUpdate(req.params.id, {$pull: {titles: req.body.title}}, (err, foundWatchlist) => {
         if (err) console.log('Error in watchlist#update:', err);
@@ -43,9 +43,24 @@ const updateRemove = (req,res) => {
     });
 }
 
+// update list by replacing with user reorder
+const updateReplace = (req,res) => {
+    const options = {new:true}
+    db.Watchlist.findByIdAndUpdate(req.params.id, req.body, options, (err, updatedWatchlist) => {
+        if (err) console.log('Error in watchlist#updateReplace:', err);
+
+        if (!updatedWatchlist) return res.json({
+            message: "Watchlist with provided ID not found"
+        });
+
+        res.status(200).json({watchlist: updatedWatchlist});
+    });
+}
+
 // exports
 module.exports = {
     show,
     update,
     updateRemove,
+    updateReplace,
 }
--- routes/watchlist.js
+++ routes/watchlist.js
@@ -6,6 +6,7 @@ const controller = require('../controllers');
 router.get('/:id', controller.watchlists.show);
 router.put('/:id/add', controller.watchlists.update);
 router.put('/:id/remove', controller.watchlists.updateRemove);
+router.put('/:id/replace', controller.watchlists.updateReplace);
 
 // exports
 module.exports = router;
