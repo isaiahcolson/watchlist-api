fdd5baf30c77ebd5ba1be136d029d7829e9b0d9f
colsonisaiah <colson.isaiah@gmail.com>
2020-07-27 16:28:38 -0400

    change jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,7 +49,7 @@ const login = async (req,res) => {
                 { _id: foundUser._id},
                 process.env.SECRET_KEY,
                 {
-                    expiresIn: '10d'
+                    expiresIn: '1h'
                 }
             );
             res.status(200).json({
--- middleware/authRequired.js
+++ middleware/authRequired.js
@@ -1,11 +1,12 @@
 // imports
 const jwt = require('jsonwebtoken');
+require("dotenv").config();
 
 // function for back end user auth
 module.exports = async (req,res,next) => {
     if (req.headers["authorization"]) {
         const token = req.headers["authorization"].split(" ")[1];
-        const payload = await jwt.verify(token, "rubberduckig11");
+        const payload = await jwt.verify(token, process.env.SECRET_KEY);
         req.currentUser = payload._id;
         next();
     } else {
