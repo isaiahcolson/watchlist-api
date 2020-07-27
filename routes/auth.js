7c2f351e05431ea6122aea8f6d7e458ac5fb6472
colsonisaiah <colson.isaiah@gmail.com>
2020-07-27 16:13:02 -0400

    removing authRequired for now

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const authRequired = require('../middleware/authRequired');
 // routes
 router.post('/register', controller.auth.register);
 router.post('/login', controller.auth.login);
-router.get('/profile', authRequired, controller.auth.profile);
+router.get('/profile', controller.auth.profile);
 router.delete('/:id', controller.auth.destroy);
 
 // exports
